e3becca24435a9a0e10477d93bb562316c8be294
ashabakshi <ashabakshi13@gmail.com>
2026-08-19 20:21:20 +0530

    Update API_BASE to Render live URL

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # =============================
 # CONFIG
 # =============================
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = "https://movie-recommendation-project-1-u6sx.onrender.com"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="🎬 HopeMatch — Movie Recommender", page_icon="🎬", layout="wide")
